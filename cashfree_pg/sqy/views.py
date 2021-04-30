649058e92ab6fab36966c6a4471efe3374bb9966
amit2181yadav <amit2181yadav@gmail.com>
2021-04-30 04:05:31 -0700

    test commit

--- cashfree_pg/sqy/views.py
+++ cashfree_pg/sqy/views.py
@@ -21,52 +21,52 @@ def orderdetails(request):
             return Response({"ordeId":orderId})
         return Response(serializer.errors)
 
-# secretKey = "<ENTER_YOUR_SECRETKEY_HERE>
-# secretKey = "5cb2d3893a2c392d8b7b045c23ea9087de2f3ca2"
-# def handlerequest(request,id):
-#     if (request.method=='GET'):
-#         mode = "TEST" # <-------Change to TEST for test server, PROD for production
-#         print(id)
-#         order = Order.objects.get(id = id)
-#         orderId = order.id  
-#         orderAmount = order.orderAmount  
-#         orderCurrency = order.orderCurrency
-#         orderNote = order.orderNote
-#         customerName = order.customerName 
-#         customerPhone = order.customerPhone  
-#         customerEmail = order.customerEmail  
-#         appId = '6604138e3302a413d0d6ab04114066'
-#         returnUrl = 'http://127.0.0.1:8000/sqy/response'
-#         notifyUrl = 'http://127.0.0.1:8000/sqy/response'
+secretKey = "<ENTER_YOUR_SECRETKEY_HERE>
+secretKey = "5cb2d3893a2c392d8b7b045c23ea9087de2f3ca2"
+def handlerequest(request,id):
+    if (request.method=='GET'):
+        mode = "TEST" # <-------Change to TEST for test server, PROD for production
+        print(id)
+        order = Order.objects.get(id = id)
+        orderId = order.id  
+        orderAmount = order.orderAmount  
+        orderCurrency = order.orderCurrency
+        orderNote = order.orderNote
+        customerName = order.customerName 
+        customerPhone = order.customerPhone  
+        customerEmail = order.customerEmail  
+        appId = '6604138e3302a413d0d6ab04114066'
+        returnUrl = 'http://127.0.0.1:8000/sqy/response'
+        notifyUrl = 'http://127.0.0.1:8000/sqy/response'
 
         
-#         postData = {
-#             "appId" : appId,
-#             "orderId" : orderId,
-#             "orderAmount" : orderAmount,
-#             "orderCurrency" : orderCurrency,
-#             "orderNote" : orderNote,
-#             "customerName" : customerName,
-#             "customerPhone" : customerPhone,
-#             "customerEmail" : customerEmail, 
-#             "returnUrl" : returnUrl,
-#             "notifyUrl" : notifyUrl
-#         }
-#         sortedKeys = sorted(postData)
-#         print(sortedKeys)
-#         signatureData = ""
-#         for key in sortedKeys:
-#             signatureData += key+str(postData[key])
-#         message = signatureData.encode('utf-8')
-#         #get secret key from your config
-#         secretKey = "5cb2d3893a2c392d8b7b045c23ea9087de2f3ca2"
-#         secret = secretKey.encode('utf-8')
-#         signature = base64.b64encode(hmac.new(secret,message,digestmod=hashlib.sha256).digest()).decode("utf-8")
-#         # signature = base64.b64encode(hmac.new(secret, message,digestmod=hashlib.sha256).digest())
-#         if mode == 'PROD': 
-#             url = "https://www.cashfree.com/checkout/post/submit"
-#         else: 
-#             url = "https://test.cashfree.com/billpay/checkout/post/submit"
-#         d = {'postData':postData,'signature':signature,'url':url}
-#         return render(request,'request.html', context = d)
-#     return render(request, 'request.html')
+        postData = {
+            "appId" : appId,
+            "orderId" : orderId,
+            "orderAmount" : orderAmount,
+            "orderCurrency" : orderCurrency,
+            "orderNote" : orderNote,
+            "customerName" : customerName,
+            "customerPhone" : customerPhone,
+            "customerEmail" : customerEmail, 
+            "returnUrl" : returnUrl,
+            "notifyUrl" : notifyUrl
+        }
+        sortedKeys = sorted(postData)
+        print(sortedKeys)
+        signatureData = ""
+        for key in sortedKeys:
+            signatureData += key+str(postData[key])
+        message = signatureData.encode('utf-8')
+        #get secret key from your config
+        secretKey = "5cb2d3893a2c392d8b7b045c23ea9087de2f3ca2"
+        secret = secretKey.encode('utf-8')
+        signature = base64.b64encode(hmac.new(secret,message,digestmod=hashlib.sha256).digest()).decode("utf-8")
+        # signature = base64.b64encode(hmac.new(secret, message,digestmod=hashlib.sha256).digest())
+        if mode == 'PROD': 
+            url = "https://www.cashfree.com/checkout/post/submit"
+        else: 
+            url = "https://test.cashfree.com/billpay/checkout/post/submit"
+        d = {'postData':postData,'signature':signature,'url':url}
+        return render(request,'request.html', context = d)
+    return render(request, 'request.html')
